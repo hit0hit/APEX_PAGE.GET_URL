f44b4c76747346a374163ece67c62a98ded11037
Dev_Hit <89605089+hit0hit@users.noreply.github.com>
2023-08-23 13:05:31 -0300

    Update code_url_V1.js

--- code_url_V1.js
+++ code_url_V1.js
@@ -1,1 +1,3 @@
 javascript: apex.server.process('GET_MODAL_URL', {x01: "#ID#"}, {     dataType:'text',     success:function(pUrl) {         eval(pUrl)     } });
+
+javascript: const inputText = '#MESSAGE#'; const cleanedText = inputText.replace(/<br\s*\/?>/g, '').replace(/:/g, '>').replace(/,/g, '<'); apex.server.process('GET_MODAL_URL', {x01: "#ID#", x02: cleanedText }, {     dataType: 'text',     success: function(pUrl) {         eval(pUrl);     } });
